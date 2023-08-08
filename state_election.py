c427ee57be40dba37f0e76f17c4507dde935ded6
junyitt <junyittc@gmail.com>
2023-08-08 22:15:55 +0800

    added model 2 (selangor invoke data)

--- state_election.py
+++ state_election.py
@@ -5,99 +5,170 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 from util import run_election_model, get_pie_fig
+from util_model_v2 import run_election_model_v2
+from PIL import Image
 
 # Streamlit main layout
 title = "Malaysia State Elections 2023"
 st.set_page_config(page_title=title, layout="wide", initial_sidebar_state="collapsed")
 st.title(title)
 
-cols_a = st.columns(6)
-with cols_a[0]:
-    state = st.selectbox("State", ['SELANGOR', 'PULAU_PINANG', 'NEGERI_SEMBILAN', 'KEDAH'])
+tab_1, tab_2, tab_appendix = st.tabs(['Model 1', 'Model 2', 'Appendix'])
 
-with cols_a[1]:
-    bn_to_ph = st.number_input('Transferability BN to PH %', min_value=0, max_value=100, value=15, step = 1)
-    bn_to_pn = st.number_input('BN to PN %', min_value=0, max_value=100, value=85, step = 1, key='bn_pn')
-    bn_to_ph /= 100
-    bn_to_pn /= 100
-    bn_to_none = 1-bn_to_ph-bn_to_pn
-    st.text(f"BN to abstain: {bn_to_none:.2%}")
+with tab_1:
+    cols_a = st.columns(6)
+    with cols_a[0]:
+        state = st.selectbox("State", ['SELANGOR', 'PULAU_PINANG', 'NEGERI_SEMBILAN', 'KEDAH'])
 
-with cols_a[2]:
-    ph_to_bn = st.number_input('Transferability PH to BN %', min_value=0, max_value=100, value=24, step = 1, key='bn_ph')
-    ph_to_pn = st.number_input('PH to PN %', min_value=0, max_value=100, value=76, step = 1, key='ph_pn')
-    ph_to_bn /= 100
-    ph_to_pn /= 100
-    ph_to_none = 1-ph_to_bn-ph_to_pn
-    st.text(f"PH to abstain %: {ph_to_none:.2%}")
+    with cols_a[1]:
+        bn_to_ph = st.number_input('Transferability BN to PH %', min_value=0, max_value=100, value=15, step = 1)
+        bn_to_pn = st.number_input('BN to PN %', min_value=0, max_value=100, value=85, step = 1, key='bn_pn')
+        bn_to_ph /= 100
+        bn_to_pn /= 100
+        bn_to_none = 1-bn_to_ph-bn_to_pn
+        st.text(f"BN to abstain: {bn_to_none:.2%}")
 
-cols_b = st.columns(6)
-with cols_b[0]:
-    show_columns = st.checkbox("Show More Data", key="show_data")
+    with cols_a[2]:
+        ph_to_bn = st.number_input('Transferability PH to BN %', min_value=0, max_value=100, value=24, step = 1, key='bn_ph')
+        ph_to_pn = st.number_input('PH to PN %', min_value=0, max_value=100, value=76, step = 1, key='ph_pn')
+        ph_to_bn /= 100
+        ph_to_pn /= 100
+        ph_to_none = 1-ph_to_bn-ph_to_pn
+        st.text(f"PH to abstain %: {ph_to_none:.2%}")
+
+    cols_b = st.columns(6)
+    with cols_b[0]:
+        show_columns = st.checkbox("Show More Data", key="show_data")
 
 
-df, scenario_df = run_election_model(state, bn_to_ph, bn_to_pn, ph_to_bn, ph_to_pn)
-df = df.rename(columns = {'STATE CONSTITUENCY CODE': 'Code', 'STATE CONSTITUENCY NAME': 'Name'})
-sdf = df.reset_index()
-if not show_columns:
-    cols_ = sdf.loc[:, 'MODEL_DUN_PN_2022':].columns.tolist()
-    sdf = sdf.loc[:, ['Code', 'Name']  + cols_].copy()
+    df, scenario_df = run_election_model(state, bn_to_ph, bn_to_pn, ph_to_bn, ph_to_pn)
+    df = df.rename(columns = {'STATE CONSTITUENCY CODE': 'Code', 'STATE CONSTITUENCY NAME': 'Name'})
+    sdf = df.reset_index()
+    if not show_columns:
+        cols_ = sdf.loc[:, 'MODEL_DUN_PN_2022':].columns.tolist()
+        sdf = sdf.loc[:, ['Code', 'Name']  + cols_].copy()
 
-# Create a function to format float columns as 1 decimal percentage
-def format_float_as_percentage(value):
-    return '{:.1%}'.format(value)
+    # Create a function to format float columns as 1 decimal percentage
+    def format_float_as_percentage(value):
+        return '{:.1%}'.format(value)
 
-styled_df = sdf.set_index(['Code', 'Name']).style.format({col: format_float_as_percentage for col in sdf.select_dtypes(include='float').columns})
+    styled_df = sdf.set_index(['Code', 'Name']).style.format({col: format_float_as_percentage for col in sdf.select_dtypes(include='float').columns})
 
-# Define a function to map categories to colors
-def map_category_to_color(category):
-    color_map = {
-        'PN': '#A7F59C',   # Light green
-        'PH': '#FF8F8F',   # Light red
-        'Unity': '#FF7399',  # Light gray
-        'BN': '#9DD8FF'    # Light blue
-    }
-    return 'background-color: {}'.format(color_map.get(category, ''))
-  
-# Apply the mapping to the Category column
-winner_cols = sdf.columns[sdf.columns.str.startswith('Winner')]
-styled_df = styled_df.applymap(map_category_to_color, subset=winner_cols)
+    # Define a function to map categories to colors
+    def map_category_to_color(category):
+        color_map = {
+            'PN': '#A7F59C',   # Light green
+            'PH': '#FF8F8F',   # Light red
+            'Unity': '#FF7399',  # Light gray
+            'BN': '#9DD8FF'    # Light blue
+        }
+        return 'background-color: {}'.format(color_map.get(category, ''))
+    
+    # Apply the mapping to the Category column
+    winner_cols = sdf.columns[sdf.columns.str.startswith('Winner')]
+    styled_df = styled_df.applymap(map_category_to_color, subset=winner_cols)
 
-majority_cols = sdf.columns[sdf.columns.str.startswith('Majority')]
-styled_df = styled_df.background_gradient(subset=majority_cols,  cmap='Reds_r', vmin=0, vmax=0.1)
+    majority_cols = sdf.columns[sdf.columns.str.startswith('Majority')]
+    styled_df = styled_df.background_gradient(subset=majority_cols,  cmap='Reds_r', vmin=0, vmax=0.1)
 
-st.dataframe(styled_df)
-# st.dataframe(scenario_df)
+    st.dataframe(styled_df)
+    # st.dataframe(scenario_df)
 
-# Pie charts
-fig_a0 = get_pie_fig(scenario_df['Winner_FED_2018'], title='Federal 2018')
-fig_a1 = get_pie_fig(scenario_df['Winner_FED_2018'], title='Federal 2022')
-fig_a2 = get_pie_fig(scenario_df['Winner_DUN_2018'], title='DUN 2018')
+    # Pie charts
+    fig_a0 = get_pie_fig(scenario_df['Winner_FED_2018'], title='Federal 2018')
+    fig_a1 = get_pie_fig(scenario_df['Winner_FED_2018'], title='Federal 2022')
+    fig_a2 = get_pie_fig(scenario_df['Winner_DUN_2018'], title='DUN 2018')
 
-fig_0 = get_pie_fig(scenario_df['Winner_MODEL_DUN_2022'], title='Base Model - Three-way (DUN2023)')
-# fig_1 = get_pie_fig(scenario_df['Winner_S1'], title='S1 - Three-Way (DUN2022)')
+    fig_0 = get_pie_fig(scenario_df['Winner_MODEL_DUN_2022'], title='Base Model - Three-way (DUN2023)')
+    # fig_1 = get_pie_fig(scenario_df['Winner_S1'], title='S1 - Three-Way (DUN2022)')
 
-fig_2a = get_pie_fig(scenario_df['Winner_S2A'], title='S2A - PH Only (DUN2023)')
-fig_2b = get_pie_fig(scenario_df['Winner_S2B'], title='S2B - BN Only (DUN2023)')
-fig_2 = get_pie_fig(scenario_df['Winner_S2'], title='S2 - Optimal Unity (DUN2023)')
+    fig_2a = get_pie_fig(scenario_df['Winner_S2A'], title='S2A - PH Only (DUN2023)')
+    fig_2b = get_pie_fig(scenario_df['Winner_S2B'], title='S2B - BN Only (DUN2023)')
+    fig_2 = get_pie_fig(scenario_df['Winner_S2'], title='S2 - Optimal Unity (DUN2023)')
 
-if show_columns:
-    cols_f2 = st.columns(3)
-    with cols_f2[0]:
-        st.plotly_chart(fig_a0, use_container_width=True)
-    with cols_f2[1]:
-        st.plotly_chart(fig_a1, use_container_width=True)
-    with cols_f2[2]:
-        st.plotly_chart(fig_a2, use_container_width=True)
+    if show_columns:
+        cols_f2 = st.columns(3)
+        with cols_f2[0]:
+            st.plotly_chart(fig_a0, use_container_width=True)
+        with cols_f2[1]:
+            st.plotly_chart(fig_a1, use_container_width=True)
+        with cols_f2[2]:
+            st.plotly_chart(fig_a2, use_container_width=True)
 
-cols_f1 = st.columns([4, 1, 2, 2, 4])
-with cols_f1[0]:
-    st.plotly_chart(fig_0, use_container_width=True)
-with cols_f1[1]:
-    st.text('')
-with cols_f1[2]:
-    st.plotly_chart(fig_2a, use_container_width=True)
-with cols_f1[3]:
-    st.plotly_chart(fig_2b, use_container_width=True)
-with cols_f1[4]:
+    cols_f1 = st.columns([4, 1, 2, 2, 4])
+    with cols_f1[0]:
+        st.plotly_chart(fig_0, use_container_width=True)
+    with cols_f1[1]:
+        st.text('')
+    with cols_f1[2]:
+        st.plotly_chart(fig_2a, use_container_width=True)
+    with cols_f1[3]:
+        st.plotly_chart(fig_2b, use_container_width=True)
+    with cols_f1[4]:
         st.plotly_chart(fig_2, use_container_width=True)
+
+
+with tab_2:
+    st.title("Selangor")
+    cols_c1 = st.columns([3,3,3])
+    with cols_c1[0]:
+        tilt_malay = st.number_input('Undecided to Unity (Malay)%', min_value=0, max_value=100, value=0, step = 1, key='tilt_1')
+    with cols_c1[1]:
+        tilt_chinese = st.number_input('Undecided to Unity (Chinese)%', min_value=0, max_value=100, value=50, step = 1, key='tilt_2')
+    with cols_c1[2]:
+        tilt_indian = st.number_input('Undecided to Unity (Indian)%', min_value=0, max_value=100, value=50, step = 1, key='tilt_3')
+    tilt = [tilt_malay/100, tilt_chinese/100, tilt_indian/100]
+    cols_t1 = st.columns([3,3,3])
+    with cols_t1[0]:
+        turnout_malay = st.number_input('Turnout (Malay)%', min_value=0, max_value=100, value=85-3, step = 1, key='turnout_1')
+    with cols_t1[1]:
+        turnout_chinese = st.number_input('Turnout (Chinese)%', min_value=0, max_value=100, value=81-18, step = 1, key='turnout_2')
+    with cols_t1[2]:
+        turnout_indian = st.number_input('Turnout (Indian)%', min_value=0, max_value=100, value=92-14, step = 1, key='turnout_3')
+    turnout = [turnout_malay/100, turnout_chinese/100, turnout_indian/100]
+    df_v2 = run_election_model_v2(tilt, turnout)
+    df_v2.index.name = 'Code'
+
+    tr_n = df_v2['Turnout_N'].sum()/df_v2['N'].sum()
+    count0 = (df_v2['Model_1'] == 'Unity').sum()
+    count1 = (df_v2['Model_2'] == 'Unity').sum()
+    count2 = (df_v2['Model_2 (2018)'] == 'Unity').sum()
+    count3 = (df_v2['Model_Invoke'] == 'Unity').sum()
+    st.text(f'Turnout: {tr_n:.2%}')
+    st.text(f'Model 1 - Unity: {count0}, PN: {56-count0}')
+    st.text(f'Model 2 - Unity: {count1}, PN: {56-count1}')
+    st.text(f'Model 2 (2018) - Unity: {count2}, PN: {56-count2}')
+    st.text(f'Invoke - Unity: {count3}, PN: {56-count3}')
+
+    def format_float_as_percentage(value):
+        return '{:.1%}'.format(value/100)
+    df_v2 = df_v2.drop(columns=['Turnout_N'])
+    styled_df2 = df_v2.style.format({col: format_float_as_percentage for col in df_v2.select_dtypes(include='float').columns})
+
+    # Define a function to map categories to colors
+    def map_category_to_color(category):
+        color_map = {
+            'PN': '#A7F59C',   # Light green
+            'PH': '#FF8F8F',   # Light red
+            'Unity': '#FF7399',  # Light gray
+            'BN': '#9DD8FF'    # Light blue
+        }
+        return 'background-color: {}'.format(color_map.get(category, ''))
+    
+    # Apply the mapping to the Category column
+    winner_cols = df_v2.columns[df_v2.columns.str.startswith('Model')]
+    styled_df2 = styled_df2.applymap(map_category_to_color, subset=winner_cols)
+    
+    st.dataframe(styled_df2, height=50*40)
+
+
+
+with tab_appendix:
+
+    image = Image.open('./appendix/turnout.jpg')
+    st.title("Turnout")
+    st.image(image, caption='Source: Invoke')
+    st.title("Support")
+    image = Image.open('./appendix/support.jpg')
+    st.image(image, caption='Source: Invoke')
+    
---
+++ util_model_v2.py
@@ -0,0 +1,173 @@
+import pandas as pd
+import plotly.graph_objects as go
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import HuberRegressor, LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
+# from util_model_v1 import run_election_model
+from util import run_election_model
+
+# PH vs PN
+# PH, PN, Unsure
+malay_1 = [0.39, 0.53, 0.08]
+chinese_1 = [0.62, 0.09, 0.29]
+indian_1 = [0.68, 0.21, 0.11]
+
+# BN vs PN
+# BN, PN, Unsure
+malay_2 = [0.35, 0.57, 0.07]
+chinese_2 = [0.41, 0.28, 0.31]
+indian_2 = [0.46, 0.37, 0.17]
+
+
+def compute_outcome(unity_party, num_malay, num_chinese, num_indian, tilt=[0.5, 0.5, 0.5], turnout = [0.79, 0.69, 0.78]):
+    tilt_malay, tilt_chinese, tilt_indian = tilt
+    turnout_malay, turnout_chinese, turnout_indian = turnout
+    # turnout_malay, turnout_chinese, turnout_indian = 0.8,0.9,0.9
+    if unity_party == "PH":
+        malay_k, chinese_k, indian_k = malay_1.copy(), chinese_1.copy(), indian_1.copy()
+    elif unity_party == "BN":
+        malay_k, chinese_k, indian_k = malay_2.copy(), chinese_2.copy(), indian_2.copy()
+    else:
+        raise
+
+    malay_unity = malay_k[0]+malay_k[2]*(tilt_malay)
+    chinese_unity = chinese_k[0]+chinese_k[2]*(tilt_chinese)
+    indian_unity = indian_k[0]+indian_k[2]*(tilt_indian)
+
+    malay_pn = malay_k[1]+malay_k[2]*(1-tilt_malay)
+    chinese_pn = chinese_k[1]+chinese_k[2]*(1-tilt_chinese)
+    indian_pn = indian_k[1]+indian_k[2]*(1-tilt_indian)
+    # print(malay_unity, chinese_unity, indian_unity)
+    # print(malay_pn, chinese_pn, indian_pn)
+    
+    unity_votes = num_malay*(malay_unity)*turnout_malay + num_chinese*(chinese_unity)*turnout_chinese + num_indian*(indian_unity)*turnout_indian
+    pn_votes = num_malay*(malay_pn)*turnout_malay + num_chinese*(chinese_pn)*turnout_chinese + num_indian*(indian_pn)*turnout_indian
+    if unity_votes > pn_votes:
+        winner = 'Unity'
+    else:
+        winner = 'PN'
+    d = {
+        'UN_M': num_malay*(malay_unity)*turnout_malay,
+        'UN_C':  num_chinese*(chinese_unity)*turnout_chinese ,
+        'UN_I': num_indian*(indian_unity)*turnout_indian,
+        'PN_M': num_malay*(malay_pn)*turnout_malay, 
+        'PN_C': num_chinese*(chinese_pn)*turnout_chinese, 
+        'PN_I': num_indian*(indian_pn)*turnout_indian, 
+        'Turnout': unity_votes+pn_votes,
+        'Majority': abs(unity_votes-pn_votes),
+        'UN': unity_votes,
+        'PN': pn_votes,
+        'Winner': winner,
+    }
+    return d
+
+
+def run_election_model_v2(tilt, turnout, skip_model_1=False):
+    # tilt=[0.0, 0.5, 0.5]
+    # tilt=[0.0, 0.5, 0.5]
+    # turnout = [0.79, 0.69, 0.78]
+    # turnout = [0.9, 0.6, 0.7]
+    dun_comp_df = pd.read_csv('data/SELANGOR_2023_DUN_COMPOSITION_custom_done.csv')
+    dun_comp_df['STATE CONSTITUENCY CODE'] = dun_comp_df['STATE CONSTITUENCY CODE'].apply(lambda x: x.replace(" ", ""))
+    dun_comp_df = dun_comp_df.set_index('STATE CONSTITUENCY CODE')
+    df18 = pd.read_csv("data/MALAYSIA_2018_DUN_COMPOSITION_IN_PROGRESS.csv").query("STATE == 'SELANGOR'")
+    df18['STATE CONSTITUENCY CODE'] = df18['STATE CONSTITUENCY CODE'].apply(lambda x: x.replace(" ", ""))
+    df18 = df18.groupby('STATE CONSTITUENCY CODE').first()
+    dun_comp_df['N'] = dun_comp_df['TOTAL ELECTORS (2023)']
+    dun_comp_df['M_18'] = df18['MALAY (%)']
+    dun_comp_df['C_18'] = df18['CHINESE (%)']
+    dun_comp_df['I_18'] = df18['INDIANS (%)']
+    dun_comp_df['O_18'] = df18['OTHERS (%)']
+    dun_comp_df['Bumi'] = dun_comp_df[['ORANG ASLI (%)', 'BUMIPUTERA SABAH (%)', 'BUMIPUTERA SARAWAK (%)']].sum(axis=1)
+    dun_comp_df['Malay'] = dun_comp_df['MALAY (%)']
+    dun_comp_df['Chinese'] = dun_comp_df['CHINESE (%)']
+    dun_comp_df['Indian'] = dun_comp_df['INDIANS (%)']
+    dun_comp_df['Other'] = dun_comp_df['OTHERS (%)']
+    dun_comp_df['Young1'] = dun_comp_df['18-20 (%)']
+    dun_comp_df['Young2'] = dun_comp_df['18-20 (%)'] + dun_comp_df['21-29 (%)']
+    dun_comp_sub_df = dun_comp_df[['STATE CONSTITUENCY NAME', 'Invoke', 'N', 'Bumi', 'M_18', 'Malay', 'C_18', 'Chinese', 'I_18', 'Indian', 'O_18', 'Other', 'Young1', 'Young2', 'Party 2', 'Party 3']].copy()
+    for i in dun_comp_sub_df.index:
+        d = dun_comp_sub_df.loc[i, :]
+        x = compute_outcome(unity_party=d['Party 2'], num_malay=d['M_18'], num_chinese=d['C_18'], num_indian=d['I_18'], tilt=tilt, turnout=turnout)
+        dun_comp_sub_df.loc[i, 'Model_2_2018'] = x['Winner']
+
+    for i in dun_comp_sub_df.index:
+        d = dun_comp_sub_df.loc[i, :]
+        x = compute_outcome(unity_party=d['Party 2'], num_malay=d['Malay'], num_chinese=d['Chinese'], num_indian=d['Indian'], tilt=tilt, turnout=turnout)
+        for key, val in x.items():
+            dun_comp_sub_df.loc[i, key] = val
+    dun_comp_sub_df['Turnout_N'] = dun_comp_sub_df['Turnout']/100*dun_comp_sub_df['N']
+
+    state = 'Selangor'
+    lost_faith = 0
+    bn_to_ph = 0.15
+    bn_to_pn = 0.85
+    ph_to_bn = 0.24
+    ph_to_pn = 0.76
+
+    if not skip_model_1:
+        # kdf, scenario_df = run_election_model(state, lost_faith, bn_to_ph, ph_to_bn)
+        kdf, scenario_df = run_election_model(state, bn_to_ph, bn_to_pn, ph_to_bn, ph_to_pn)
+        dun_comp_sub_df['Model_1'] = kdf.set_index('STATE CONSTITUENCY CODE')['Winner_S2']
+    dun_comp_sub_df['Model_2'] = dun_comp_sub_df['Winner']
+    dun_comp_sub_df['Model_2 (2018)'] = dun_comp_sub_df['Model_2_2018']
+    dun_comp_sub_df['Model_Invoke'] = dun_comp_sub_df['Invoke']
+    return dun_comp_sub_df.drop(columns=['Winner', 'Model_2_2018', 'Invoke'])
+
+
+def get_pie_fig(sr, title):
+    # Create a sample data with precalculated percentage values
+    x = sr.sort_values(ascending=False).dropna()
+    labels = x.index
+    values = x.values
+
+    # Define color mapping
+    color_map = {
+        'PN': '#85E579',    # Slightly darker green
+        'PH': '#FF7171',    # Slightly darker red
+        'Unity': '#FF84A3',  # Slightly darker gray
+        'BN': '#82C9FF'     # Slightly darker blue
+    }
+    color_map = {
+        'PN': '#65D25C',    # Darker green
+        'PH': '#FF5E5E',    # Darker red
+        'Unity': '#FF7399',  # Darker gray
+        'BN': '#70BFFF'     # Darker blue
+    }
+    # color_map = {
+    #     'PN': '#A7F59C',   # Light green
+    #     'PH': '#FF8F8F',   # Light red
+    #     'Unity': '#FF9DBB',  # Light gray
+    #     'BN': '#9DD8FF'    # Light blue
+    # }
+
+    # Get colors from color mapping
+    colors = [color_map[label] for label in labels]
+
+    # Create the Donut Chart trace
+    trace = go.Pie(
+        labels=labels,
+        values=values,
+        marker=dict(colors=colors),
+        textinfo='value',
+        hoverinfo='label+percent',
+        hole=0.4,
+        textfont_size=20,
+    )
+
+    # Create the layout for the Donut Chart
+    layout = go.Layout(
+        title=title,
+        title_font=dict(size=24, color='#333'),
+        width=600,
+        height=600,
+        margin=dict(t=100, b=0, l=0, r=0),
+        hoverlabel=dict(font=dict(size=20))
+    )
+
+    # Create the Figure object
+    fig = go.Figure(data=[trace], layout=layout)
+
+    # Display the Donut Chart
+    # fig.show()
+    return fig
